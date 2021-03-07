ecc575692c95ff4c8c118782a8c45bcd9a354716
Nicolas <nicolasnigno@gmail.com>
2021-03-07 12:18:23 -0500

    updates

---
+++ Medical Images/gradient_magnitude_grayscale/build/CMakeFiles/gmg.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gmg.dir/gmg.cxx.o"
+  "gmg"
+  "gmg.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gmg.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
