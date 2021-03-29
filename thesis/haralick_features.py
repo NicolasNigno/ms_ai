0b48040a8194edac4837f77d5c82f96340e7104c
Nicolas <nicolasnigno@gmail.com>
2021-03-28 19:58:20 -0500

    new functions + better old functions
    vectorized almost everything

--- thesis/haralick_features.py
+++ thesis/haralick_features.py
@@ -24,24 +24,10 @@ def r_diagonalGTSDM(window, levels):
 def ASM(matrix):
     return np.sum(matrix**2)
 
-def constrast(matrix):
-    matrixSum = 0
+def constrastMatrix(matrix):
+    support_matrix = np.fromfunction(lambda i, j: (i-j)**2, shape = matrix.shape,)
     
-    for i in range(len(matrix)):
-        for j in range(len(matrix)):
-            matrixSum += ((i-j)**2)*matrix[i][j]
-    
-    return matrixSum
-
-def correlation_(matrix, mean_x,  mean_y, std_x, std_y):
-    matrixSum = 0
-    
-    for i in range(len(matrix)):
-        for j in range(len(matrix)):
-            ## ES NECESARIO SUMARLE 1 A LOS ÍNDICES??? NO LO SÉ
-            matrixSum += ((i+1) - mean_x) * ((j+1) - mean_y) * matrix[i][j] 
-    
-    return matrixSum/(std_x * std_y)
+    return np.multiply(matrix, support_matrix).sum()
 
 def getMeans(matrix):
     mean_x = matrix.sum(axis=1) @ np.array(range(len(matrix)))
@@ -55,42 +41,45 @@ def getStd(matrix, mean_x, mean_y):
     
     return std_x**(0.5), std_y**(0.5)
 
+def correlationMatrix_(matrix, mean_x,  mean_y, std_x, std_y):
+    support_i = np.fromfunction(lambda i, j: i + 1, shape = matrix.shape,)
+    support_j = np.fromfunction(lambda i, j: j + 1, shape = matrix.shape,)
+    
+    support_i = support_i - mean_x
+    support_j = support_j - mean_y
+    
+    return np.multiply(np.multiply(support_i, support_j), matrix).sum() / (std_x * std_y)
+
 def getCorrelation(matrix):
     mean_x, mean_y = getMeans(matrix)
     std_x, std_y = getStd(matrix, mean_x, mean_y)
-    correlation_value = correlation_(matrix, mean_x,  mean_y, std_x, std_y)
+    correlation_value = correlationMatrix_(matrix, mean_x,  mean_y, std_x, std_y)
     
     return correlation_value
 
-def variance(matrix):
+def variance(i,j):
+    return ((i+1))
+
+def varianceMatrix(matrix):
     mu = np.sum(matrix)/(len(matrix)**2)
-    variance = 0
+    support_matrix = np.fromfunction(variance, shape = matrix.shape, )
+    support_matrix = support_matrix - mu
+    support_matrix = support_matrix**2
     
-    for i in range(len(matrix)):
-        for j in range(len(matrix)):
-            ## ES NECESARIO SUMARLE 1 A LOS ÍNDICES??? NO LO SÉ
-            variance += (((i+1) - mu)**2) * (matrix[i][j])
-    
-    return variance
+    return np.multiply(matrix, support_matrix).sum()
 
-def inverseDifferenceMoment(matrix):
-    idm = 0
-    
-    for i in range(len(matrix)):
-        for j in range(len(matrix)):
-            idm += (matrix[i][j]) / (1 + ((i-j)**2))
-    
-    return idm
+def inverseDifferenceMoment(i, j):
+    return 1/(1 + ((i-j)**2))
 
-def entropy(matrix):
+def inverseDifferenceMomentMatrix(matrix):
+    support_matrix = np.fromfunction(inverseDifferenceMoment, shape = matrix.shape, )
+    
+    return np.multiply(matrix, support_matrix).sum()
+
+def entropyMatrix(matrix):
     e = 1e-30
-    entropy = 0
     
-    for i in range(len(matrix)):
-        for j in range(len(matrix)):
-            entropy += (matrix[i][j]) * np.log(matrix[i][j] + e)
-    
-    return -entropy
+    return np.multiply(matrix, np.log(matrix + e)).sum()
 
 def sumAverageMatrix(matrix):
     sumvector = []
@@ -137,3 +126,34 @@ def sumEntropyMatrix(matrix):
         sumentropy += intermediate_sum * np.log(intermediate_sum + e)
     
     return -sumentropy
+
+def Px_y(matrix):
+    array = []
+    
+    for i in range(len(matrix)):
+        if i == 0:
+            array.append(np.trace(matrix, offset=i,))
+        else:
+            array.append(np.trace(matrix, offset=i,) + np.trace(matrix, offset=-i,))
+    
+    return np.array(array)
+
+def MUx_y(matrix, px_y):
+    n = np.array(range(len(matrix)))    
+    return n @ px_y
+
+def differenceVariance(matrix):
+    p_xy = Px_y(matrix)
+    mu_xy = MUx_y(matrix, p_xy)
+    
+    n = list(range(len(matrix)))
+    n = [(x-mu_xy)**2 for x in n]
+    n = np.array(n)
+    
+    return -(n @ p_xy)
+
+def differenceEntropy(matrix):
+    e = 1e-30
+    p_xy = Px_y(matrix)
+    
+    return p_xy @ (p_xy + e)
